49b4e9e83c74d4d0837deca817d87e43811c7876
Reid Merzbacher <rmerzbacher@gmail.com>
2022-10-11 01:45:56 -0400

    added Netlify keyword

--- src/contact.js
+++ src/contact.js
@@ -30,7 +30,7 @@ const encode = (data) => {
       const { name, email, message } = this.state;
       return (
         <div className="body">
-            <form onSubmit={this.handleSubmit}>
+            <form onSubmit={this.handleSubmit} netlify>
             <p>
                 <label>
                 Name: <input type="text" name="name" value={name} onChange={this.handleChange} />
